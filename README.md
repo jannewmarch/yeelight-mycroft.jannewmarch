3dcdca221f522a37d25e785e442b1a3b59839a13
Jan Newmarch <jan@newmarch.name>
2019-01-19 17:37:09 +1100

    Changed vocab to be consistent with LIFX

--- README.md
+++ README.md
@@ -6,17 +6,13 @@ Skill to manage the Yeelight smart lights
 Yeelight make a white light and a colour bulb. This can be controlled through two ways: remotely via the Yeelight server and locally using the LAN. This project controls Yeelights using the local LAN. Note that the Yeelights have to be configured to use the local LAN - the default is off.
 
 ## Examples 
-* "Bedroom light on"
-* "Lounge light off"
-* "Light on"
-* "Light off"
+* "Turn bedroom light on"
+* "Turn lounge light off"
 
 ## Credits 
 Jan Newmarch (jan@newmarch.name)
 ## Usage
-If a location is given, the light at that location will be used. If there is no light there, the intent will fail.
-If no location is given, and there are one or more lights, then 
-the first one detected will be used.
+If there is no light at the location given, the intent will fail, otherwise it will succeed.
 
 Warning: Discovery of lights is unreliable. Reported as a bug to Yeelight, no response received. So
 sometimes this skill will fail due to bugs in Yeelight networking.
--- __init__.py
+++ __init__.py
@@ -1,148 +1,36 @@
-import string
+from unittest.mock import MagicMock
+from test.integrationtests.skills.skill_tester import SkillTest
 
-from adapt.intent import IntentBuilder
-from mycroft.skills.core import MycroftSkill, intent_handler
-from mycroft.dialog import DialogLoader
-from mycroft.util.log import getLogger
+def test_runner(skill, example, emitter, loader):
 
-import yeelight
-from yeelight import Bulb
+    # Get the skill object from the skill path
+    s = [s for s in loader.skills if s and s.root_dir == skill]
 
-__author__ = 'Jan Newmarch jan@newmarch.name'
-
-'''
-This skill manipulates a Yeelight
-
-and accepts the commands
-
-  * [location] light on
-  * [location] light off
-
-if the location is given
-  if there is a light with name of that location, use it
-  else fail
-else
-  if there is at least one light, use the first one found
-  else fail
-'''
-
-
-LOGGER = getLogger(__name__)
-
-class YeelightSkill(MycroftSkill):
-    def __init__(self):
-        super(YeelightSkill, self).__init__(name="YeelightSkill")
-
-    def initialize(self):
-        self.light_wrapper = LightWrapper()
+    # replace the light_wrapper service with a mock
+    s[0].light_wrapper = MagicMock()
     
-    @intent_handler(IntentBuilder("OnIntent").\
-                    optionally("location").\
-                    require("OnKeywords").build())
-    def on_activity_intent(self, message):
-        """Turn on Yeelight
-        """
-        
-        LOGGER.debug("Yeelight: start activity" + message.data.get('utterance'))
-
-        location = message.data.get("location")
-        if location != None:
-            LOGGER.info('Room is ' + location)
-        else:
-            LOGGER.info('No room given')
-            
-        light, name = self.select_light(location)
-        if light != None:
-            light.turn_on()
-            
-            if name == None:
-                report = {"location": "unknown"}
-            else:
-                report = {"location": name}
-                
-            self.speak_dialog("on.activity", report)
-        else:
-            self.speak_dialog("fail")
-            
-            
-
-    @intent_handler(IntentBuilder("OffIntent").\
-                    optionally("location").\
-                    require("OffKeywords").build())
-    def off_activity_intent(self, message):
-        """Turn off Yeelight
-        """
-        
-        LOGGER.debug("Yeelight: start activity" + message.data.get('utterance'))
-
-        location = message.data.get("location")
-
-        light, name = self.select_light(location)
-        if light != None:
-            light.turn_off()
+    # Set a valid return value for discover_bulbs()
+    s[0].light_wrapper.discover_bulbs.return_value = [
+        {'ip': '192.168.1.25',
+         'port': 55443,
+         'capabilities': {'name': 'bedroom'
+                        }
+        },
+        {'ip': '192.168.1.26',
+         'port': 55443,
+         'capabilities': {'name': 'lounge'
+                         }
+        }
+    ]
  
-            if name == None:
-                report = {"location": "unknown"}
-            else:
-                report = {"location": name}
-                
-            self.speak_dialog("off.activity", report)
-        else:
-            self.speak_dialog("fail")
- 
+    # Set a valid return value for new_light()
+    s[0].light_wrapper.new_light.return_value = TestBulb()
 
-    def select_light(self, location):
-        #yeelights = yeelight.discover_bulbs()
-        yeelights = self.light_wrapper.discover_bulbs()
-        LOGGER.info("Num Yeelights {}".format(len(yeelights)))
-        LOGGER.info(str(yeelights))
-        
-        if location != None:
-            for light_info in yeelights:
-                name = light_info['capabilities']['name']
-                if name == location:
-                    ip = light_info['ip']
-                    #light = Bulb(ip)
-                    light = self.light_wrapper.new_light(ip)
-                    
-                    LOGGER.info("Yeelight ip {}".format(ip))
-                    
-                    return light, name
-                
-            LOGGER.info('No light matching ' + location)
-            return None, None
-            
-        if len(yeelights) >= 1:
-            light_info = yeelights[0]
-            ip = light_info['ip']
-            name = light_info['capabilities']['name']
-            
-            LOGGER.info("Yeelight ip {}".format(ip))
-            
-            light = Bulb(ip)
-            return light, name
-        
-        return None, None
-    
+    return SkillTest(skill, example, emitter).run(loader)
 
-    def stop(self):
+class TestBulb:
+    def turn_on(self):
         pass
 
-
-class LightWrapper:
-    """ Wrapper around yeelight package so it
-    can be overriden for testing
-    """
-
-    @staticmethod
-    def discover_bulbs():
-        return yeelight.discover_bulbs(timeout=5)
-
-    @staticmethod
-    def new_light(ip):
-        return yeelight.Bulb(ip)
-    
-def create_skill():
-    return YeelightSkill()
-
-
+    def turn_off(self):
+        pass
